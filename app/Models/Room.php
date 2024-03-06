05d549fb96b4d20354f2c1bb0a59bd73b86d0612
elshodatc111 <elshodatc1116@gmail.com>
2024-03-06 21:27:45 +0300

    Save

---
+++ app/Models/Room.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model{
+    use HasFactory;
+    protected $fillable = ["filial_id","room_name","room_sigimi","room_max_sigimi","'user_id","status"];
+}
---
+++ database/migrations/2024_03_06_182254_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('filial_id');
+            $table->string('room_name');
+            $table->integer('room_sigimi');
+            $table->integer('room_max_sigimi');
+            $table->integer('user_id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
